8abd1d8f10d494d0101b5c1d3519591b0639e16f
Jimbo420 <66968943+Jimbo420@users.noreply.github.com>
2024-10-07 15:35:24 +0200

    Refactor drawSnake function to correctly create snake elements

--- Snakegame.js
+++ Snakegame.js
@@ -1,6 +1,7 @@
 //Defining HTML elements
 const board = document.getElementById('game-board');
 
+//Defining game variables
 let snake = [{ x: 10, y: 10 }];
 
 //Draw game board. map, snake food
@@ -10,8 +11,23 @@ function draw() {
 }
 
 //Draw snake on game board
-fuction drawSnake() {
+function drawSnake() {
     snake.forEach((segment) =>{
         const snakeElement = createGameElement('div', 'snake')
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
     })
+}
+
+//Create game element
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+//Set position of game element
+function setPosition(element, position) {
+    element.style.gridRow = position.y;
+    element.style.gridColumn = position.x;
 }
